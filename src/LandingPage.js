f7ebf53b74c77aa6e5cd6cb7e15abaca0a7d7a68
kpramos-hau <kpramos@student.hau.edu.ph>
2025-06-24 22:07:06 +0800

    Edited LoginPage.js and login.css

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -24,14 +24,14 @@ function LandingPage() {
                 </ul>
             </div>
                 <div className='nav-bar-login'>
-                    <button className='button-nav' type='submit' onClick={handleLogin}>Login</button>
+                    <button className='button-nav' onClick={handleLogin}>Login</button>
                 </div>
         </div>
         <div className='main-content'>
             <div className='left-hero-text'>
                 <h1 className='hero-text'>Oh, hi there!</h1>
                 <p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque delectus sequi. Eaque, deleniti accusamus. Ipsam, illum! Similique mollitia, ab voluptas eius nostrum velit, unde error minima, quod nemo recusandae.</p>
-                <button className='main-content-button'>Register</button>  {/* this button does not work */}
+                <button className='main-content-button' onClick={handleLogin} >Register</button>  {/* this button leads to the login page */}
             </div>
             <div className='right-hero-image'>
                 <img src={hero} width={500} className='hero-image'></img>
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,23 +3,26 @@ import './login.css';
 
 function LoginPage() {
  
-  return (
-    <div className='main-container'>
+    return (
+    <div className='login-page-image'>
       <div className='container'>
-        <h1>Login</h1>
-        <input
-        className='textbox'
-          type="text"
-          placeholder="Username"
-        
-        /><br />
-        <input
-          className='textbox'
-          type="password"
-          placeholder="Password"
+          <div className='login'>
+              <h1 className='login-text'>Login</h1>
+              <input className='textbox' type='text' placeholder='Username' autoComplete='off'></input>
+              <input className='textbox' type='password' placeholder='Password' autoComplete='off' ></input>
 
-        /><br />
-        <button className='login-button' >Login</button>
+            <div className='check'>
+              <div className='remember'>
+                <input type='checkbox'></input>
+                <label htmlFor="remember" className='text'>Remember me</label>
+                </div>
+                <div>
+                <strong><a id='forgot-password' href='#'>Forgot Password?</a></strong>
+              </div>
+            </div>
+              <button className='buttons' type='submit'>Login</button>
+              <p className='hero-text-2'>Don't have an account? <strong><a href='#'>Register Here.</a></strong></p>
+          </div>
       </div>
     </div>
   );
