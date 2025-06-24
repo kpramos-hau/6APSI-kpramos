502ce4db643c92fbfe6c01a96db94dd6c4223e75
kpramos-hau <kpramos@student.hau.edu.ph>
2025-06-24 20:33:43 +0800

    Added image and modified LandingPage.js and landing.css

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 import './landing.css';
+import hero from './hero.jpg'
 
 function LandingPage() {
  
   return (
-    <div className='nav-bar'>
-        <div className='nav-bar-items'>
-            <h1>Random</h1>
-            <ul>
-                <li>GitHub</li>
-                <li>Sources</li>
-                <li>About</li>
-            </ul>
-        </div>
-            <div className='nav-bar-login'>
-                <button className='button-nav'>Login</button>
+    <div >
+        <div className='nav-bar'>
+            <div className='nav-bar-items'>
+                <h1>Random</h1>
+                <ul>
+                    {/* will add links if needed, this stays as plaintext for now */}
+                    <li>GitHub</li> 
+                    <li>Sources</li>
+                    <li>About</li>
+                </ul>
             </div>
+                <div className='nav-bar-login'>
+                    <button className='button-nav'>Login</button>
+                </div>
+        </div>
+        <div className='main-content'>
+            <div className='left-hero-text'>
+                <h1 className='hero-text'>Oh, hi there!</h1>
+                <p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque delectus sequi. Eaque, deleniti accusamus. Ipsam, illum! Similique mollitia, ab voluptas eius nostrum velit, unde error minima, quod nemo recusandae.</p>
+                <button className='main-content-button'>Register</button>
+            </div>
+            <div className='right-hero-image'>
+                <img src={hero} width={500} className='hero-image'></img>
+            </div>
+        </div>
     </div>
   );
 }
